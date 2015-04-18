ebaa37189f47be0f07cd781d8088f6f430b752d3
Grace Eunjin Kim <graceeekimmm@gmail.com>
2015-04-17 18:40:38 -0700

    added mock data, csv implementation, some views for user and teams

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  require 'csv'
   has_and_belongs_to_many :courses 
   has_and_belongs_to_many :teams
+
+  def self.import(file)
+     CSV.foreach(file.path, headers: true) do |row|
+         User.create! row.to_hash
+     end
+  end
+  
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable

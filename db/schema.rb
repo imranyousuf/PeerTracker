8809034199cdd3a4c9db2d5b87e219ecd7b03d4c
Douglas <douglaskim06@gmail.com>
2015-04-27 15:59:30 -0700

    database migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150427101157) do
 
   create_table "assignments", force: :cascade do |t|

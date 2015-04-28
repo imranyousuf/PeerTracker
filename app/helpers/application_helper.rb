5b2c8f92be22e836e19d78b04a803f4e3ad3865e
Ruiqi Wang <wang.ruiqi.94@gmail.com>
2015-04-27 21:00:49 -0700

    UI for feedbacks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "success"
+      when :error
+        "error"
+      when :alert
+        "alert"
+      when :notice
+        "notice"
+      else
+        flash_type.to_s
+    end
+  end
 end

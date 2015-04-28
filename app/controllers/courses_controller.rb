3c7cf7d243381fcb77a212d13d6ac62541941c5d
phjaykwon <phjaykwon93@gmail.com>
2015-04-28 00:59:04 -0700

    simple ui for login/signup

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -16,7 +16,6 @@ class CoursesController < ApplicationController
   # GET /courses/1.json
   def show
     puts params[:id]
-    puts "HEREEEEEEEE"
     @course = Course.find(params[:id])
     if current_user.has_role? :student or current_user.has_role? :instructor
       redirect_to course_teams_path(:course_id => @course.id)
@@ -39,7 +38,7 @@ class CoursesController < ApplicationController
   # POST /courses
   # POST /courses.json
   def create
-    @course = current_user.courses.new(course_params)
+    @course = Course.new(course_params)
     @course.user_id = @current_user.user_id
     respond_to do |format|
       #instructor_exists

9666567e7332eea71872d063fb848b67e728c806
phjaykwon <phjaykwon93@gmail.com>
2015-05-04 20:40:14 -0700

    changed navbar ui, fixed delete

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,11 @@
 class Course < ActiveRecord::Base
 	has_many :assignments
 	has_and_belongs_to_many :users, :join_table => :courses_users
-	has_many :teams
+	has_many :teams, dependent: :destroy
 	has_one :credential
   validates :course_name, uniqueness: { message: "Course name exists"}
   validate :format_name
+
 
   def format_name
     if /[A-Z]{1,} [0-9A-Z]+/.match(course_name) == nil
